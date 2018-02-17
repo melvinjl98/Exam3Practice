b57b5b51766fd870e129558949e4220d41f07408
melvinjl <melvinjl@rose-hulman.edu>
2018-02-17 15:51:20 -0500

    m3 done

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  FOR and WHILE loops.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Josh Melvin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -45,7 +45,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ####################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -109,6 +109,8 @@ def run_test_practice_problem3():
              st.SimpleTestCase(practice_problem3,
                                [0, 1, 1.414213562373],
                                [286602]),
+             st.SimpleTestCase(practice_problem3, [1, 0, 22], []),
+             st.SimpleTestCase(practice_problem3, [0, 2, -1], [0, 1])
              ]
     # 14th test:
     big_list = []
@@ -208,7 +210,7 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ####################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ####################################################################
@@ -216,7 +218,18 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ####################################################################
-
+    list = []
+    while True:
+        if n == 0:
+            return []
+        if threshold > math.sqrt(2):
+            for k in range(start, n):
+                list.append(k)
+        if math.cos(start) + math.sin(start) > threshold:
+            list.append(start)
+        if len(list) == n:
+            return list
+        start = start + 1
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
